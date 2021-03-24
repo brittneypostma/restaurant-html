4925bfa605a05afa92680014a8ba97448cd0a076
brittneypostma <brittney.postma@hotmail.com>
2021-03-24 15:19:15 -0400

    init

---
+++ src/js/load-products.js
@@ -0,0 +1,79 @@
+const data = [
+  {
+    "id": 1,
+    "categories": ["ramen"],
+    "name": "Veggie Ramen",
+    "descriptionShort": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger.",
+    "descriptionLong": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger. Yokohama soy milk soy milk seasoned egg ground black pepper chopped onions roasted pork slices spinach seasoned egg kamaboko chicken stock pork bones sesame oil chicken stock Hakata minced garlic Wakayama ramen burger toasted sesame seeds. Abura soba abura soba butter Hakata ramen burger Hakata Kumamoto tsukemen curry Asahikawa. Soy milk Kagoshima yuzu butter salt Nissin instant cup ramen Wakayama soy sauce chicken stock Hakata toasted sesame seeds ramen burger flavoured oil. Seasoned egg Tokushima Sapporo butter ramen burger minced garlic Tokyo wood ear mushroom soy milk bamboo slices Asahikawa Nagoya pork bones Kagoshima leek vinegar Kagoshima Tokyo leek. Kagoshima lard Asahikawa mustard greens Sapporo yuzu fish broth wood ear mushroom Wakayama tsukemen soy milk yuzu nori yuzu curry Tokyo bean sprouts Kagoshima corn. Soy sauce pork bones fish broth Tokushima chilli vinegar vinegar nori vinegar soy sauce.",
+    "price": "12.00",
+    "image": "veggie"
+  },
+  {
+    "id": 2,
+    "categories": ["ramen"],
+    "name": "Shrimp Ramen",
+    "descriptionShort": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger.",
+    "descriptionLong": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger. Yokohama soy milk soy milk seasoned egg ground black pepper chopped onions roasted pork slices spinach seasoned egg kamaboko chicken stock pork bones sesame oil chicken stock Hakata minced garlic Wakayama ramen burger toasted sesame seeds. Abura soba abura soba butter Hakata ramen burger Hakata Kumamoto tsukemen curry Asahikawa. Soy milk Kagoshima yuzu butter salt Nissin instant cup ramen Wakayama soy sauce chicken stock Hakata toasted sesame seeds ramen burger flavoured oil. Seasoned egg Tokushima Sapporo butter ramen burger minced garlic Tokyo wood ear mushroom soy milk bamboo slices Asahikawa Nagoya pork bones Kagoshima leek vinegar Kagoshima Tokyo leek. Kagoshima lard Asahikawa mustard greens Sapporo yuzu fish broth wood ear mushroom Wakayama tsukemen soy milk yuzu nori yuzu curry Tokyo bean sprouts Kagoshima corn. Soy sauce pork bones fish broth Tokushima chilli vinegar vinegar nori vinegar soy sauce.",
+    "price": "15.00",
+    "image": "shrimp"
+  },
+  {
+    "id": 3,
+    "categories": ["ramen"],
+    "name": "Protein Ramen",
+    "descriptionShort": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger.",
+    "descriptionLong": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger. Yokohama soy milk soy milk seasoned egg ground black pepper chopped onions roasted pork slices spinach seasoned egg kamaboko chicken stock pork bones sesame oil chicken stock Hakata minced garlic Wakayama ramen burger toasted sesame seeds. Abura soba abura soba butter Hakata ramen burger Hakata Kumamoto tsukemen curry Asahikawa. Soy milk Kagoshima yuzu butter salt Nissin instant cup ramen Wakayama soy sauce chicken stock Hakata toasted sesame seeds ramen burger flavoured oil. Seasoned egg Tokushima Sapporo butter ramen burger minced garlic Tokyo wood ear mushroom soy milk bamboo slices Asahikawa Nagoya pork bones Kagoshima leek vinegar Kagoshima Tokyo leek. Kagoshima lard Asahikawa mustard greens Sapporo yuzu fish broth wood ear mushroom Wakayama tsukemen soy milk yuzu nori yuzu curry Tokyo bean sprouts Kagoshima corn. Soy sauce pork bones fish broth Tokushima chilli vinegar vinegar nori vinegar soy sauce.",
+    "price": "15.00",
+    "image": "protein"
+  },
+  {
+    "id": 4,
+    "categories": ["ramen"],
+    "name": "Pork Bacon Ramen",
+    "descriptionShort": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger.",
+    "descriptionLong": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger. Yokohama soy milk soy milk seasoned egg ground black pepper chopped onions roasted pork slices spinach seasoned egg kamaboko chicken stock pork bones sesame oil chicken stock Hakata minced garlic Wakayama ramen burger toasted sesame seeds. Abura soba abura soba butter Hakata ramen burger Hakata Kumamoto tsukemen curry Asahikawa. Soy milk Kagoshima yuzu butter salt Nissin instant cup ramen Wakayama soy sauce chicken stock Hakata toasted sesame seeds ramen burger flavoured oil. Seasoned egg Tokushima Sapporo butter ramen burger minced garlic Tokyo wood ear mushroom soy milk bamboo slices Asahikawa Nagoya pork bones Kagoshima leek vinegar Kagoshima Tokyo leek. Kagoshima lard Asahikawa mustard greens Sapporo yuzu fish broth wood ear mushroom Wakayama tsukemen soy milk yuzu nori yuzu curry Tokyo bean sprouts Kagoshima corn. Soy sauce pork bones fish broth Tokushima chilli vinegar vinegar nori vinegar soy sauce.",
+    "price": "12.00",
+    "image": "pork"
+  },
+  {
+    "id": 5,
+    "categories": ["ramen"],
+    "name": "Chicken Ramen",
+    "descriptionShort": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger.",
+    "descriptionLong": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger. Yokohama soy milk soy milk seasoned egg ground black pepper chopped onions roasted pork slices spinach seasoned egg kamaboko chicken stock pork bones sesame oil chicken stock Hakata minced garlic Wakayama ramen burger toasted sesame seeds. Abura soba abura soba butter Hakata ramen burger Hakata Kumamoto tsukemen curry Asahikawa. Soy milk Kagoshima yuzu butter salt Nissin instant cup ramen Wakayama soy sauce chicken stock Hakata toasted sesame seeds ramen burger flavoured oil. Seasoned egg Tokushima Sapporo butter ramen burger minced garlic Tokyo wood ear mushroom soy milk bamboo slices Asahikawa Nagoya pork bones Kagoshima leek vinegar Kagoshima Tokyo leek. Kagoshima lard Asahikawa mustard greens Sapporo yuzu fish broth wood ear mushroom Wakayama tsukemen soy milk yuzu nori yuzu curry Tokyo bean sprouts Kagoshima corn. Soy sauce pork bones fish broth Tokushima chilli vinegar vinegar nori vinegar soy sauce.",
+    "price": "12.00",
+    "image": "chicken"
+  },
+  {
+    "id": 6,
+    "categories": ["ramen"],
+    "name": "Spicy Beef Ramen",
+    "descriptionShort": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger.",
+    "descriptionLong": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger. Yokohama soy milk soy milk seasoned egg ground black pepper chopped onions roasted pork slices spinach seasoned egg kamaboko chicken stock pork bones sesame oil chicken stock Hakata minced garlic Wakayama ramen burger toasted sesame seeds. Abura soba abura soba butter Hakata ramen burger Hakata Kumamoto tsukemen curry Asahikawa. Soy milk Kagoshima yuzu butter salt Nissin instant cup ramen Wakayama soy sauce chicken stock Hakata toasted sesame seeds ramen burger flavoured oil. Seasoned egg Tokushima Sapporo butter ramen burger minced garlic Tokyo wood ear mushroom soy milk bamboo slices Asahikawa Nagoya pork bones Kagoshima leek vinegar Kagoshima Tokyo leek. Kagoshima lard Asahikawa mustard greens Sapporo yuzu fish broth wood ear mushroom Wakayama tsukemen soy milk yuzu nori yuzu curry Tokyo bean sprouts Kagoshima corn. Soy sauce pork bones fish broth Tokushima chilli vinegar vinegar nori vinegar soy sauce.",
+    "price": "12.00",
+    "image": "beef"
+  }
+]
+
+
+function createTemplate(item) {
+  const template = document.querySelector('#menu')
+  const product = template.content.cloneNode(true)
+
+  // const img = product.querySelector('img')
+  // img.src = item.product.images[0]
+  // img.alt = item.product.name
+  product.querySelector('h3').innerText = item.name
+  product.querySelector('.desc').innerText = item.descriptionShort
+  product.querySelector('.price').innerText = `$${item.price }`
+
+
+  return product
+}
+
+export async function loadProducts() {
+  const products = document.querySelector('#products')
+  data.forEach((product) => {
+    products.appendChild(createTemplate(product))
+  })
+}
---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  purge:  [
+    './src/**/*.html',
+    './src/**/*.js',
+    ]
+,
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    //! anything here overrides tailwinds defaults //
+    colors: {
+      white: '#ffffff',
+      lightGray: '#fefefe',
+      black: "#111111",
+      red: "#A50022",
+      darkRed: "#640A13",
+      orange: "#FF7800",
+      darkOrange: "#e97711",
+      transparent: "transparent"
+    },
+    fontFamily: {
+      heading: "'Merienda One', sans-serif",
+      body: "'Advent Pro', sans-serif",
+    },
+    extend: {
+      //! anything here extends tailwind defaults //
+      boxShadow: {
+        outline: '0 0 0 3px rgba(0,0,0, 0.5)',
+        inset: 'inset 0px 5px 10px rgba(0, 0, 0, 0.25)',
+        none: 'none'
+      },
+      fontSize: {
+        '8xl': "8rem"
+      },
+      scale: {
+        '-1': '-1'
+      }
+    },
+  },
+  variants: {
+    //! add in variants that don't come in by default //
+    //! order is important //
+    backgroundColor: ["responsive", "hover", "focus", "active"],
+    borderWidth: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+    scale: ['responsive', 'hover', 'focus', 'active', 'group-hover']
+  },
+  plugins: [],
+}
