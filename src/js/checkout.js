cf159e8db3ecaa706bd776e18111be131e3d9c60
brittneypostma <brittney.postma@hotmail.com>
2021-04-22 15:16:34 -0400

    moved out usc logic

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,144 +1,153 @@
-//test api key
-// const testApiKey = "pk_test_51H3WzVA5gnA92gtxXNCulxWBYSBrAt4xvuuYkv6Tp5uvlQl1ZPZj3uHIX0PNCSDjdP7eOv7o46nlKZzhuvoowggt00VRkn0FMf";
-// let store = UseShoppingCartCore.createShoppingCartStore({
-//   stripe: testApiKey,
-//   mode: "client-only",
-//   successUrl: "https://success.html",
-//   cancelUrl: "https://index.html",
-// });
+// * Use Shopping Cart
+const testApiKey =
+  'pk_test_51H3WzVA5gnA92gtxXNCulxWBYSBrAt4xvuuYkv6Tp5uvlQl1ZPZj3uHIX0PNCSDjdP7eOv7o46nlKZzhuvoowggt00VRkn0FMf'
+let store = UseShoppingCartCore.createShoppingCartStore({
+  stripe: testApiKey,
+  mode: 'client-only',
+  successUrl: 'https://success.html',
+  cancelUrl: 'https://index.html',
+})
 
-// let persistor = UseShoppingCartCore.createPersistedStore(store);
+let persistor = UseShoppingCartCore.createPersistedStore(store)
 
+const {
+  addItem,
+  checkoutSingleItem,
+  clearCart,
+  decrementItem,
+  handleCartClick,
+  handleCloseCart,
+  incrementItem,
+  redirectToCheckout,
+  removeItem,
+  formatCurrencyString,
+} = UseShoppingCartCore.actions
 
-// const {
-//   addItem,
-//   checkoutSingleItem,
-//   clearCart,
-//   decrementItem,
-//   handleCartClick,
-//   handleCloseCart,
-//   incrementItem,
-//   redirectToCheckout,
-//   removeItem,
-//   formatCurrencyString,
-// } = UseShoppingCartCore.actions;
+function loadCart() {
+  let cartCount = store.getState().cartCount
+  const itemCount = document.getElementById('item-count')
+  const cartTitle = document.getElementById('cart-title')
+  if (!store.getState().bootstrapped) {
+    itemCount.innerHTML = '?'
+  }
+  if (cartCount === 0) {
+    document.getElementById('cart-items').classList.add('hidden')
+    cartTitle.innerHTML = `Oh the sadness, you don't have any ramen in your cart!`
+  }
+  if (cartCount > 0) {
+    cartTitle.innerHTML = `Your Order`
+    document.getElementById('cart-items').classList.remove('hidden')
+  }
 
-// let numberOfItems = Object.keys(JSON.stringify(store.getState().cartDetails)).length === 0 ? JSON.stringify(store.getState().cartDetails) : 0
+  document.getElementById('checkout').addEventListener('click', function () {
+    store.dispatch(redirectToCheckout())
+  })
+  loadItems()
 
-// function render() {
-//   if (!store.getState().bootstrapped) {
-//     document.getElementById("cart-details").innerHTML = "?";
-//   }
+  itemCount.innerHTML = cartCount
+}
+loadCart()
 
-//   document.getElementById("cart-details").innerHTML = numberOfItems;
+store.subscribe(loadCart)
 
+function loadItems() {
+  const orderDetails = document.querySelector('#order-details')
+  const cartList = document.querySelector('#cart-list')
+  const receipt = document.querySelector('#receipt')
+  cartList.innerHTML = ''
+  orderDetails.innerHTML = ''
+  receipt.classList.add('hidden')
+  const items = Object.keys(store.getState().cartDetails).map(
+    item => store.getState().cartDetails[item]
+  )
+  items.forEach(item => {
+    // *list of items in cart
+    cartList.insertAdjacentHTML(
+      'afterbegin',
+      `
+        <li
+          class="flex gap-4 p-5 items-start text-black bg-white border-t-4 border-solid rounded-sm shadow-2xl border-orange justify-stretch  justify-between"
+        >
+          <div class="grid gap-4">
+            <h2 class="text-2xl text-left whitespace-nowrap lg:text-4xl">${
+              item.name
+            }</h2>
+            <p class="lg:text-2xl">Total: ${item.formattedValue} / $${(
+        Math.round(item.price) / 100
+      ).toFixed(2)} each</p>
+          </div>
+          <div class="grid justify-items-end h-full content-between">
+            <button onclick="store.dispatch(removeItem('${item.id}'))">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                />
+              </svg>
+            </button>
+            <div class="flex items-center">
+              <button
+                class="flex items-center justify-center w-8 h-8 text-3xl transition-colors bg-white border-2 border-solid cursor-pointer text-red border-red hover:bg-red hover:text-white"
+                aria-label="remove one ${item.name} from cart"
+                onclick="store.dispatch(decrementItem('${item.id}'))"
+              >
+                -
+              </button>
+              <label for="number"
+                ><input
+                  class="w-8 h-8 mx-5 text-2xl text-center text-white rounded-full bg-red font-heading"
+                  type="number"
+                  min="1"
+                  value="${item.quantity}"
+              /></label>
+              <button
+                class="flex items-center justify-center w-8 h-8 text-3xl transition-colors bg-white border-2 border-solid cursor-pointer text-red border-red hover:bg-red hover:text-white"
+                aria-label="add one ${item.name} from cart"
+                onclick="store.dispatch(incrementItem('${item.id}'))"
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </li>
+`
+    )
+    if (items.length > 0) {
+      document.querySelector('#receipt').classList.remove('hidden')
+      // *list of items in order details
+      orderDetails.insertAdjacentHTML(
+        'afterbegin',
+        `
+        <li
+        class="pt-2 flex gap-4 items-start bg-white border-t border-solid border-orange justify-stretch  justify-between"
+        >
+          <div class="grid gap-4 w-full">
+            <div class="flex justify-between">
+              <h4 class="text-left whitespace-nowrap">${item.name}</h4>
+              <p class="text-lg lg:text-2xl font-heading">${item.formattedValue}</p>
+            </div>
+            <p class="lg:text-2xl justify-self-end">Qty. ${item.quantity}</p>
+          </div>
+        </li>
+`
+      )
 
-// }
-// render();
+      // * total
+      document.querySelector(
+        '#total'
+      ).innerHTML = store.getState().formattedTotalPrice
+    }
 
-// store.subscribe(render);
-
-
-
-// function addItemToCart(item) {
-
-// }
-
-// document
-//   .getElementById("addBananasToCart")
-//   .addEventListener("click", function () {
-//     store.dispatch(addItem(product));
-//   });
-
-// document
-//   .getElementById("addTangerinesToCart")
-//   .addEventListener("click", function () {
-//     store.dispatch(addItem(product2));
-//   });
-
-// document
-//   .getElementById("clearCart")
-//   .addEventListener("click", function () {
-//     store.dispatch(clearCart());
-//   });
-
-// document
-//   .getElementById("checkout")
-//   .addEventListener("click", function () {
-//     store.dispatch(redirectToCheckout());
-//   });
-
-
-// import stripe
-// pull products from stripe
-// load products from stripe
-// update cart with usc
-// redirect to stripe
-
-// const products = [
-//   {
-//     "id": 1,
-//     "categories": ["ramen"],
-//     "name": "Veggie Ramen",
-//     "desc": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger.",
-//     "price": "12.00",
-//     "image": "veggie"
-//   },
-//   {
-//     "id": 2,
-//     "categories": ["ramen"],
-//     "name": "Shrimp Ramen",
-//     "desc": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger.",
-//     "price": "15.00",
-//     "image": "shrimp"
-//   },
-//   {
-//     "id": 3,
-//     "categories": ["ramen"],
-//     "name": "Protein Ramen",
-//     "desc": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger.",
-//     "price": "15.00",
-//     "image": "protein"
-//   },
-//   {
-//     "id": 4,
-//     "categories": ["ramen"],
-//     "name": "Pork Bacon Ramen",
-//     "desc": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger.",
-//     "price": "12.00",
-//     "image": "pork"
-//   },
-//   {
-//     "id": 5,
-//     "categories": ["ramen"],
-//     "name": "Chicken Ramen",
-//     "desc": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger.",
-//     "price": "12.00",
-//     "image": "chicken"
-//   },
-//   {
-//     "id": 6,
-//     "categories": ["ramen"],
-//     "name": "Spicy Beef Ramen",
-//     "desc": "Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger.",
-//     "price": "12.00",
-//     "image": "beef"
-//   }
-// ]
-
-// const product = {
-//   name: "Bananas",
-//   description: "Yummy yellow fruit",
-//   id: "sku_J4vwzv3Z8kx5JD",
-//   price: 400,
-//   currency: "USD",
-//   image: "https://my-image.com/banana.jpg",
-// };
-
-// const product2 = {
-//   name: "Tangerines",
-//   id: "sku_J4vwzA4WWceluX",
-//   price: 100,
-//   image: "https://i.imgur.com/4rVhatT.jpg",
-//   currency: "USD",
-// };
+    if (store.getState().cartCount === 0) {
+      recept.classList.add('hidden')
+    }
+  })
+}
--- src/js/load-products.js
+++ src/js/load-products.js
@@ -1,157 +1,3 @@
-// * Use Shopping Cart
-const testApiKey =
-  'pk_test_51H3WzVA5gnA92gtxXNCulxWBYSBrAt4xvuuYkv6Tp5uvlQl1ZPZj3uHIX0PNCSDjdP7eOv7o46nlKZzhuvoowggt00VRkn0FMf'
-let store = UseShoppingCartCore.createShoppingCartStore({
-  stripe: testApiKey,
-  mode: 'client-only',
-  successUrl: 'https://success.html',
-  cancelUrl: 'https://index.html',
-})
-
-let persistor = UseShoppingCartCore.createPersistedStore(store)
-
-const {
-  addItem,
-  checkoutSingleItem,
-  clearCart,
-  decrementItem,
-  handleCartClick,
-  handleCloseCart,
-  incrementItem,
-  redirectToCheckout,
-  removeItem,
-  formatCurrencyString,
-} = UseShoppingCartCore.actions
-
-function loadCart() {
-  let cartCount = store.getState().cartCount
-  const itemCount = document.getElementById('item-count')
-  const cartTitle = document.getElementById('cart-title')
-  if (!store.getState().bootstrapped) {
-    itemCount.innerHTML = '?'
-  }
-  if (cartCount === 0) {
-    document.getElementById('cart-items').classList.add('hidden')
-    cartTitle.innerHTML = `Oh the sadness, you don't have any ramen in your cart!`
-  }
-  if (cartCount > 0) {
-    cartTitle.innerHTML = `Your Order`
-    document.getElementById('cart-items').classList.remove('hidden')
-  }
-
-  document.getElementById('checkout').addEventListener('click', function () {
-    store.dispatch(redirectToCheckout())
-  })
-  loadItems()
-
-  itemCount.innerHTML = cartCount
-}
-loadCart()
-
-store.subscribe(loadCart)
-
-function loadItems() {
-  const orderDetails = document.querySelector('#order-details')
-  const cartList = document.querySelector('#cart-list')
-  const receipt = document.querySelector('#receipt')
-  cartList.innerHTML = ''
-  orderDetails.innerHTML = ''
-  receipt.classList.add('hidden')
-  const items = Object.keys(store.getState().cartDetails).map(
-    item => store.getState().cartDetails[item]
-  )
-  items.forEach(item => {
-    // *list of items in cart
-    cartList.insertAdjacentHTML(
-      'afterbegin',
-      `
-        <li
-          class="flex gap-4 p-5 items-start text-black bg-white border-t-4 border-solid rounded-sm shadow-2xl border-orange justify-stretch  justify-between"
-        >
-          <div class="grid gap-4">
-            <h2 class="text-2xl text-left whitespace-nowrap lg:text-4xl">${
-              item.name
-            }</h2>
-            <p class="lg:text-2xl">Total: ${item.formattedValue} / $${(
-        Math.round(item.price) / 100
-      ).toFixed(2)} each</p>
-          </div>
-          <div class="grid justify-items-end h-full content-between">
-            <button onclick="store.dispatch(removeItem('${item.id}'))">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                />
-              </svg>
-            </button>
-            <div class="flex items-center">
-              <button
-                class="flex items-center justify-center w-8 h-8 text-3xl transition-colors bg-white border-2 border-solid cursor-pointer text-red border-red hover:bg-red hover:text-white"
-                aria-label="remove one ${item.name} from cart"
-                onclick="store.dispatch(decrementItem('${item.id}'))"
-              >
-                -
-              </button>
-              <label for="number"
-                ><input
-                  class="w-8 h-8 mx-5 text-2xl text-center text-white rounded-full bg-red font-heading"
-                  type="number"
-                  min="1"
-                  value="${item.quantity}"
-              /></label>
-              <button
-                class="flex items-center justify-center w-8 h-8 text-3xl transition-colors bg-white border-2 border-solid cursor-pointer text-red border-red hover:bg-red hover:text-white"
-                aria-label="add one ${item.name} from cart"
-                onclick="store.dispatch(incrementItem('${item.id}'))"
-              >
-                +
-              </button>
-            </div>
-          </div>
-        </li>
-`
-    )
-    if (items.length > 0) {
-      document.querySelector('#receipt').classList.remove('hidden')
-      // *list of items in order details
-      orderDetails.insertAdjacentHTML(
-        'afterbegin',
-        `
-        <li
-        class="pt-2 flex gap-4 items-start bg-white border-t border-solid border-orange justify-stretch  justify-between"
-        >
-          <div class="grid gap-4 w-full">
-            <div class="flex justify-between">
-              <h4 class="text-left whitespace-nowrap">${item.name}</h4>
-              <p class="text-lg lg:text-2xl font-heading">${item.formattedValue}</p>
-            </div>
-            <p class="lg:text-2xl justify-self-end">Qty. ${item.quantity}</p>
-          </div>
-        </li>
-`
-      )
-
-      // * total
-      document.querySelector(
-        '#total'
-      ).innerHTML = store.getState().formattedTotalPrice
-    }
-
-    if (store.getState().cartCount === 0) {
-      recept.classList.add('hidden')
-    }
-  })
-}
-
 function getItemDetails(item) {
   return {
     name: item.product.name,
@@ -197,7 +43,7 @@ function createOrderTemplate(item) {
   return order
 }
 
-async function loadProducts() {
+export async function loadProducts() {
   const menu = document.querySelector('#menu')
   const order = document.querySelector('#order')
   const prices = await fetch('/.netlify/functions/get-products')
@@ -209,8 +55,6 @@ async function loadProducts() {
     order.appendChild(createOrderTemplate(price))
   })
 }
-
-loadProducts()
 
 async function handleFormSubmit(event) {
   event.preventDefault()
