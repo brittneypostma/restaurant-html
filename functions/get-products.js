d4bb97267ce620cda537a64dc62cf87f15ad3ece
brittneypostma <brittney.postma@hotmail.com>
2021-04-27 15:53:22 -0400

    added logs to get-products

--- functions/get-products.js
+++ functions/get-products.js
@@ -3,12 +3,13 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 exports.handler = async (event, context, callback) => {
   try {
     const prices = await stripe.prices.list({ expand: ['data.product'] })
+    console.log(prices)
     return {
       statusCode: 200,
       body: JSON.stringify(prices),
     }
   } catch (error) {
-    console.log('Something went wrong.')
+    console.log(`Something went wrong: ${error}`)
     console.error(error)
   }
 }
